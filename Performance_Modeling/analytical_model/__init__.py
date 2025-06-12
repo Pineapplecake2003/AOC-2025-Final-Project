89699404ab07a1aded565ee73d6fe73ede64ff30
dennis <dennis356811@gmail.com>
2025-06-13 01:25:01 +0800

    delete unnecesary file

--- Performance_Modeling/analytical_model/__init__.py
+++
@@ -1,4 +0,0 @@
-from .eyeriss import EyerissAnalyzer, EyerissHardwareParam
-from .mapper import EyerissMapper
-
-__all__ = ["EyerissAnalyzer", "EyerissHardwareParam", "EyerissMapper"]
--- TVM/Python/utils/__init__.py
+++
@@ -1,5 +0,0 @@
-from .utils import *
-from .fuse import *
-from .codegen import *
-from .note import *
-from .datagen import *
