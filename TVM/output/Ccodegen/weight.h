64b68a34a614f74f884601998cb59e3986f64711
henrae1001 <96062908+henrae1001@users.noreply.github.com>
2025-06-16 16:21:38 +0800

    output for model_relay and generalized file

---
+++ TVM/output/Ccodegen/weight.h
@@ -0,0 +1,44 @@
+#ifndef DATA_H
+#define DATA_H
+
+#include <stdint.h>
+
+extern int8_t tvmgen_default_DLA_main_0_const_0[];
+extern int32_t tvmgen_default_DLA_main_0_const_1[];
+extern int8_t tvmgen_default_DLA_main_0_const_2[];
+extern int32_t tvmgen_default_DLA_main_0_const_3[];
+extern int8_t tvmgen_default_DLA_main_0_const_4[];
+extern int32_t tvmgen_default_DLA_main_0_const_5[];
+extern int8_t tvmgen_default_DLA_main_0_const_6[];
+extern int32_t tvmgen_default_DLA_main_0_const_7[];
+extern int8_t tvmgen_default_DLA_main_0_const_8[];
+extern int32_t tvmgen_default_DLA_main_0_const_9[];
+extern int8_t tvmgen_default_DLA_main_0_const_10[];
+extern int32_t tvmgen_default_DLA_main_0_const_11[];
+extern int8_t tvmgen_default_DLA_main_0_const_12[];
+extern int32_t tvmgen_default_DLA_main_0_const_13[];
+extern int8_t tvmgen_default_DLA_main_0_const_14[];
+extern int32_t tvmgen_default_DLA_main_0_const_15[];
+extern int8_t tvmgen_default_DLA_main_0_const_16[];
+extern int32_t tvmgen_default_DLA_main_0_const_17[];
+extern int8_t tvmgen_default_DLA_main_0_const_18[];
+extern int32_t tvmgen_default_DLA_main_0_const_19[];
+extern int8_t tvmgen_default_DLA_main_0_const_20[];
+extern int32_t tvmgen_default_DLA_main_0_const_21[];
+extern int8_t tvmgen_default_DLA_main_0_const_22[];
+extern int32_t tvmgen_default_DLA_main_0_const_23[];
+extern int8_t tvmgen_default_DLA_main_0_const_24[];
+extern int32_t tvmgen_default_DLA_main_0_const_25[];
+extern int8_t tvmgen_default_DLA_main_0_const_26[];
+extern int32_t tvmgen_default_DLA_main_0_const_27[];
+extern int8_t tvmgen_default_DLA_main_0_const_28[];
+extern int32_t tvmgen_default_DLA_main_0_const_29[];
+extern int8_t tvmgen_default_DLA_main_0_const_30[];
+extern int32_t tvmgen_default_DLA_main_0_const_31[];
+extern int8_t tvmgen_default_DLA_main_0_const_32[];
+extern int32_t tvmgen_default_DLA_main_0_const_33[];
+extern int8_t tvmgen_default_DLA_main_0_const_34[];
+extern int32_t tvmgen_default_DLA_main_0_const_35[];
+void load_weight(const char *filename);
+
+#endif // DATA_H
